0a288709b4970b63751371e48fada07f00fbe39c
Felipe Aron Nudelman Tabacinik Reichmann <aronakamoto@outlook.com>
2023-02-17 18:42:20 -0300

    Refactor: bot running

--- deploy_bot_discord.py
+++ deploy_bot_discord.py
@@ -1,22 +1,34 @@
 import discord
 
-client = discord.Client()
+intents = discord.Intents.all()
+
+client = discord.Client(intents=intents)
 
 @client.event
-async def on_ready(self):
-    print(f'Logged in as {self.user} (ID: {self.user.id})')
-    print('------')
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name='Serverzin')
+    channel = discord.utils.get(guild.text_channels, name='geral')
+    await channel.send('The bot is online!')
 
 @client.event
-async def on_message(self, message):
-    # we do not want the bot to reply to itself
-    if message.author.id == self.user.id:
-        return
+async def on_message(message):
+    if isinstance(message.channel, discord.DMChannel):
+        if message.content.lower() == '!hello':
+            await message.channel.send('Hello, this is the Equal Chatbot in private.')
 
-    if message.content.startswith('!source'):
-        await message.reply('The link for the source code of the bot is here in Github: https://github.com/aronfelipe/equal-chatbot/blob/main/deploy_bot_discord.py!', mention_author=True)
+        if message.content.startswith('!source'):
+            await message.reply('The link for the source code of the bot is here in Github: https://github.com/aronfelipe/equal-chatbot/blob/main/deploy_bot_discord.py!', mention_author=False)
 
-    if message.content.startswith('!author'):
-        await message.reply('The chatbot author email is: felipe.nudelman@gmail.com and his name is Felipe Aron', mention_author=True)
+        if message.content.startswith('!author'):
+            await message.reply('The chatbot author email is: felipe.nudelman@gmail.com and his name is Felipe Aron', mention_author=True)
+    else:
+        if message.content.lower() == '!hello':
+            await message.channel.send('Hello, this is the Equal Chatbot in public!')
+
+        if message.content.startswith('!source'):
+            await message.reply('The link for the source code of the bot is here in Github: https://github.com/aronfelipe/equal-chatbot/blob/main/deploy_bot_discord.py!', mention_author=False)
+
+        if message.content.startswith('!author'):
+            await message.reply('The chatbot author email is: felipe.nudelman@gmail.com and his name is Felipe Aron', mention_author=True)
 
 client.run('')
