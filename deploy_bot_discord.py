6869e70f8973a44ddb1d1de0dadfba8d7434a213
Felipe Aron Nudelman Tabacinik Reichmann <aronakamoto@outlook.com>
2023-02-17 15:31:14 -0300

    Feat: updated bot

--- deploy_bot_discord.py
+++ deploy_bot_discord.py
@@ -1,22 +1,22 @@
 import discord
 
+client = discord.Client()
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+@client.event
+async def on_ready(self):
+    print(f'Logged in as {self.user} (ID: {self.user.id})')
+    print('------')
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
+@client.event
+async def on_message(self, message):
+    # we do not want the bot to reply to itself
+    if message.author.id == self.user.id:
+        return
 
-        if message.content.startswith('!source'):
-            await message.reply('The link for the source code of the bot is here in Github: !', mention_author=True)
+    if message.content.startswith('!source'):
+        await message.reply('The link for the source code of the bot is here in Github: https://github.com/aronfelipe/equal-chatbot/blob/main/deploy_bot_discord.py!', mention_author=True)
 
+    if message.content.startswith('!author'):
+        await message.reply('The chatbot author email is: felipe.nudelman@gmail.com and his name is Felipe Aron', mention_author=True)
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = MyClient(intents=intents)
-client.run('token')
+client.run('')
